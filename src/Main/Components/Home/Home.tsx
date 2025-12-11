5a9803347a8558bb6c678307ca02642db6e8911e
JL-Huawei_Graznov <graznov@me.com>
2025-12-11 18:30:10 +0300

    11.12.25 - home

--- src/Main/Components/Home/Home.tsx
+++ src/Main/Components/Home/Home.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './home.module.css';
+import {TEMP_VALLUES} from "../../../TEMP.ts";
 
 const cx = classNames.bind(styles);
 
@@ -8,7 +9,22 @@ function Home() {
     return (
         <div className={cx('home')}>
 
-            Home
+            <div className="home_content">
+                <div className="home_content_Left">
+                    <div className="zagol">
+                        {TEMP_VALLUES.ZAGOL}
+                    </div>
+                    <div className="text">
+                        {TEMP_VALLUES.LONG_TEXT}
+                    </div>
+                </div>
+
+                <div className="home_content_Right">
+                    <div className="text">
+                        {TEMP_VALLUES.LONG_TEXT}
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
