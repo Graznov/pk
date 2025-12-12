639e1d664c0f5951a90642f8db30f1664b9d4e34
JL-Huawei_Graznov <graznov@me.com>
2025-12-12 21:03:39 +0300

    12.12.25 videos

--- src/Main/Components/Fabrication/Fabrication.tsx
+++ src/Main/Components/Fabrication/Fabrication.tsx
@@ -7,28 +7,41 @@ const cx = classNames.bind(styles)
 function Fabrication() {
 
     return(
+
         <div className={cx('fabrication')}>
             <div className={cx("content")}>
 
                 <div className={cx("home_content")}>
-                    {/*<div className={cx("home_content_Left")}>*/}
-                        <div className={cx("zagol")}>
-                            {TEMP_VALLUES.FABRICATION.ZAGOL}
-                        </div>
-                    {/*    <div className={cx("text")}>*/}
-                    {/*        {TEMP_VALLUES.SERVICE.LONG_TEXT_1}*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className={cx("zagol")}>
+                        {TEMP_VALLUES.FABRICATION.ZAGOL}
+                    </div>
 
-                    {/*<div className={cx("home_content_Right")}>*/}
-                    {/*    <div className={cx("text")}>*/}
-                    {/*        {TEMP_VALLUES.SERVICE.LONG_TEXT_2}*/}
-                    {/*    </div>*/}
-                    {/*</div>*/}
+                    <div className={cx("videos")}>
+
+                        <iframe className={cx("videos_item")} src="https://www.youtube.com/embed/y4fCbHMtvhc"
+                                title="Zip wallet / Кошелек на молнии из кожи от #wildleathercraft. Free pattern PDF."
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+
+                        <iframe className={cx("videos_item")} src="https://www.youtube.com/embed/Er49UHlEnI0"
+                                title="Making a Bi-fold wallet from Pull-Up leather Tobacco by #wildleathercraft. Free pattern PDF."
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+
+                        <iframe className={cx("videos_item")} src="https://www.youtube.com/embed/sBtuixpCZ_A"
+                                title="Making a Handmade Traditional Bifold Wallet." frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen></iframe>
+
+                    </div>
                 </div>
 
             </div>
-
 
 
         </div>
